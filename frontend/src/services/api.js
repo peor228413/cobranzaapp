ce44072e5da6b250d10ef5c13919b983370e1129
Nestor Chaparro <zonicness.nac@gmail.com>
2025-08-07 18:26:14 -0600

    instalación vite y pruebas con proxy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,5 @@
-const BASE_URL = "http://localhost:3000"; // Cambia de puerto si usas otro
-
 export async function getClientes() {
-  const response = await fetch(`${BASE_URL}/clientes`);
+  const response = await fetch("/api/clientes");
 
   if (!response.ok) {
     throw new Error("Error al obtener clientes");
